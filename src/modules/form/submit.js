c2d930bc7bc1cabf654ffb88a29109f0983f4737
Renato <renatosilsoa@gmail.com>
2026-03-29 15:19:25 -0300

    Impedir seleção de datas anteriores à atual faturado

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -6,6 +6,9 @@ const selectedDate = document.getElementById("date")
 //Carrega a data atual.
 selectedDate.value = dayjs(new Date()).format("YYYY-MM-DD")
 
+//Definir a data mínima como sendo a data atual
+selectedDate.min = dayjs( new Date()).format("YYYY-MM-DD")
+
 form.onsubmit = (event) => {
     //Previne o comportamento padrão de carregamento a página
     event.preventDefault()
