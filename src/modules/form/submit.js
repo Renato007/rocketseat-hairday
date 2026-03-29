f639197ddfcd43128c2b5f40f795bd306621a678
Renato <renatosilsoa@gmail.com>
2026-03-29 15:22:00 -0300

    fatoração

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -3,11 +3,14 @@ import dayjs from "dayjs";
 const form = document.querySelector('form');
 const selectedDate = document.getElementById("date")
 
+//Date atual para formatar o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
 //Carrega a data atual.
-selectedDate.value = dayjs(new Date()).format("YYYY-MM-DD")
+selectedDate.value = inputToday
 
 //Definir a data mínima como sendo a data atual
-selectedDate.min = dayjs( new Date()).format("YYYY-MM-DD")
+selectedDate.min = inputToday
 
 form.onsubmit = (event) => {
     //Previne o comportamento padrão de carregamento a página
