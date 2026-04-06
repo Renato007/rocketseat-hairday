2150919f1497cbbd20d5db819afedd0190893d54
Renato Soares <renatosilsoa@gmail.com>
2026-04-06 17:54:43 -0300

    Renderizando os horários

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -2,6 +2,8 @@ import dayjs from "dayjs";
 import { openingHours } from "../../utils/opening-hours.js";
 
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({ date }) {
   console.log(date);
   const opening = openingHours.map((hour) => {
@@ -16,6 +18,15 @@ export function hoursLoad({ date }) {
       available: isHourPast,
     };
   });
-  console.log(opening); 
-  
+
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li");
+
+    li.classList.add("hour");
+    li.classList.add(available ? "hour-available" : "hour-unavaible")
+
+    li.textContent = hour
+
+    hours.append(li)
+  });
 }
