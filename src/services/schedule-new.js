05b1b41a35cb1febc07ba35fce10736f1647fcc3
Renato Soares <renatosilsoa@gmail.com>
2026-04-08 10:19:14 -0300

    Criando API-config e Criando o serviço de agendamento

---
+++ src/services/schedule-new.js
@@ -0,0 +1,24 @@
+import {apiConfig} from "./api-config.js"
+
+export async function scheduleNew({id, name, when}) {
+    try {
+        //Requisição da API apontando a rota/recurso
+        await fetch(`${apiConfig.baseURL}/schedule`, {
+            //ação
+            method: "POST",
+
+            //descrição do tipo de conteúdo
+            headers: {
+                "Content-Type": "application/json",
+            },
+            //
+            body:JSON.stringify({id, name, when})
+        })
+
+        alert("Agendamento realizado com sucesso!")
+    } catch (error) {
+        console.log(error);
+        alert("Não foi possível agendar. Tente novamente mais tarde.")
+        
+    }
+} 
